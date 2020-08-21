bd41ce9371b91c03eee752eb3c69579cca1b258d
Ивахненко В.М <ivahnenkovm@ugpa.ru>
2020-08-21 17:04:15 +0300

    default

--- index.js
+++ index.js
@@ -6,23 +6,13 @@ const dotenv = require("dotenv")
 dotenv.config()
 const PORT = process.env.PORT || 4000
 const app = express()
-const jwt = require('jsonwebtoken')
 const fileUpload = require('express-fileupload');
 app.use(express.static('./files'));
 app.use(fileUpload());
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/verify/:id/', (req, res) => {
-    jwt.verify(req.params.id, process.env.TOKEN_SECRET, async (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log('token verified successful')
-            app.use('api/todos', require('./routes/router'))
-        }
-    }).catch(error => { console.log(error) })
-});
+app.use('/api/todos', require('./routes/router'))
 async function start() {
     try {
         await mongoose.connect(process.env.MONGODB_URL, {
